4daf1b437c01dc62be4c8280a5afa780a13f0641
TheSnazzmaster <43121657+TheSnazzmaster@users.noreply.github.com>
2020-11-14 11:38:15 -0800

    added player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public float gravity = -0.1f;
     public float jumpForce = 1f;
+    public float moveForce = 1f;
+    public float groundMaxSpeed = 1f;
+    public float airModifier = 0.005f;
+
 
     private BoxCollider2D collider;
     private Rigidbody2D rb;
@@ -40,7 +44,6 @@ public class PlayerMovement : MonoBehaviour
             acc.y = 0;
             vel.y = 0;
         }
-
         transform.position += new Vector3(vel.x,vel.y);
         vel += acc;
         wasOnGround = onGround;
@@ -51,12 +54,41 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") > 0.5 && onGround)
         {
             vel.y = jumpForce;
-            onGround = false;
         }
+        if (onGround)
+        {
+            vel.x = Input.GetAxis("Horizontal") * moveForce;
+        }
+        else
+        {
+            vel.x = Input.GetAxis("Horizontal") * moveForce* airModifier;
+        }
+        /*  bad movement
+        acc.x = Input.GetAxisRaw("Horizontal") * moveForce;
+        if (onGround)
+        {
+            if (vel.x > groundMaxSpeed)
+                vel.x = groundMaxSpeed;
+            if (vel.x < -groundMaxSpeed)
+                vel.x = -groundMaxSpeed;
+        }
+        else
+        {
+            float airModifier = 1.2f;
+            if (vel.x > groundMaxSpeed*airModifier)
+                vel.x = groundMaxSpeed * airModifier;
+            if (vel.x < -groundMaxSpeed * airModifier)
+                vel.x = -groundMaxSpeed * airModifier;
+        }
+        */
         rb.MovePosition(transform.position);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         onGround = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        onGround = false;
     }
 }
